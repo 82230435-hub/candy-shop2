a7afaf1b709d1955e929e4182f95dba24867993d
82230435-hub <82230435@students.liu.edu.lb>
2025-12-31 13:11:50 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,13 +15,16 @@ app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // Database
+require('dotenv').config(); // أعلى الملف
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "candyshop",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   multipleStatements: true
 });
+
 
 // Create database and tables if not exist
 const initDB = `
